dbf6d4fadcc74d1353b1feb04fb513ab1a740eaa
YuriFreitas1 <yuri.freitasdasilva2@gmail.com>
2025-12-16 20:03:57 -0300

    feat: criação do model clients

--- clients/models.py
+++ clients/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 
-# Create your models here.
+class cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.CharField(blank= True,max_length=50)
+    data_criacao =models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
