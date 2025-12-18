21e083055ccc06ce5944b49c61304602bd0d40f1
YuriFreitas1 <yuri.freitasdasilva2@gmail.com>
2025-12-17 22:12:25 -0300

    feat: validações de negócio no agendamento

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -6,3 +6,4 @@ class AgendamentosAdmin(admin.ModelAdmin):
     list_display = ('cliente', 'servico', 'data_hora', 'status')
     list_filter = ('status', 'data_hora')
     search_fields = ('cliente__nome', 'servico__nome')
+    ordering = ('data_hora',)
--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 from servicos.models import Servico
 from clientes.models import Cliente
 
@@ -35,3 +37,14 @@ class Agendamento(models.Model):
 
     def __str__(self):
         return f'{self.cliente} - {self.servico} - {self.data_hora}'
+    
+    def clean(self):
+        if self.data_hora < timezone.now():
+            raise ValidationError("Não é possível criar agendamento no passado.")
+        
+    def clean(self):
+        if Agendamento.objects.filter(
+            data_hora=self.data_hora,
+            servico=self.servico
+        ).exclude(pk=self.pk).exists():
+            raise ValidationError("Já existe um agendamento para esse horário.")
